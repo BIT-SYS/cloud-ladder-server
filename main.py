f4890dd3defd0429624d2234236682e751802ff4
wuzirui <wuzirui@tztyun.org>
2022-07-17 09:00:02 +0800

    text to image, functional tests didn't pass due to the lack of calculating power😓

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from flask import Flask, request
-import model_driver.ImageCaptioning as im2text
-import model_driver.SpeechRecognition as sr
+# import model_driver.ImageCaptioning as im2text
+# import model_driver.SpeechRecognition as sr
+import model_driver.TextGeneration as text2img
 
 
 app = Flask(__name__)
@@ -12,22 +13,28 @@ def get_test_msg():
     return "Hello World"
 
 
-# 图片转文字，使用post请求上传图片，图片数据保存在image字段中
-@app.route('/im2text', methods=['POST'])
-def get_caption_text():
-    img = request.files.get('image')
-    # 调用分装好的推理函数
-    return im2text.inference(img)
+# # 图片转文字，使用post请求上传图片，图片数据保存在image字段中
+# @app.route('/im2text', methods=['POST'])
+# def get_caption_text():
+#     img = request.files.get('image')
+#     # 调用分装好的推理函数
+#     return im2text.inference(img)
 
-# 语音转文字，使用post请求上传wav音频，音频数据保存在audio_file字段中，识别的语言类型保存在lang中
-@app.route('/speech_recognition', methods=['POST'])
-def get_speech():
-    audio_file = request.files.get('audio')
-    lang =  request.form.get('lang')
-    print(lang)
-    if (lang != 'en-US') and (lang != 'zh-CN'):
-        return 'warning:不支持的语言识别'
-    return sr.inference(audio_file,lang)
+# # 语音转文字，使用post请求上传wav音频，音频数据保存在audio_file字段中，识别的语言类型保存在lang中
+# @app.route('/speech_recognition', methods=['POST'])
+# def get_speech():
+#     audio_file = request.files.get('audio')
+#     lang =  request.form.get('lang')
+#     print(lang)
+#     if (lang != 'en-US') and (lang != 'zh-CN'):
+#         return 'warning:不支持的语言识别'
+#     return sr.inference(audio_file,lang)
+
+# 从POST请求中读取字符串，调用Dall-E Mini模型进行推理，将推理结果封装在file中返回
+@app.route('/text2image', methods=['POST'])
+def get_generated_image():
+    text = request.form.get('text')
+    return text2img.inference(text)
 
 if __name__ == "__main__":
     # 启动服务器，运行在5000端口上
